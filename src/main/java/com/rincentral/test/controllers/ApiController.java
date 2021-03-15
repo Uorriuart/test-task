3d6858d6b2338c643e48c6bb546d944c30a4fc70
Uorri <uorriart@yandex.ru>
2021-03-15 18:50:18 +0300

    Adding functionality and validation to get methods

--- src/main/java/com/rincentral/test/controllers/ApiController.java
+++ src/main/java/com/rincentral/test/controllers/ApiController.java
@@ -3,53 +3,74 @@ package com.rincentral.test.controllers;
 import com.rincentral.test.models.CarInfo;
 import com.rincentral.test.models.params.CarRequestParameters;
 import com.rincentral.test.models.params.MaxSpeedRequestParameters;
+import com.rincentral.test.services.api.CarService;
+import com.rincentral.test.services.api.SpeedService;
+import com.rincentral.test.services.api.TypeCarService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
 import java.util.List;
-
-import static java.util.Collections.emptyList;
 
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
 public class ApiController {
 
+    CarService service;
+    TypeCarService typesCarService;
+    SpeedService speedService;
+
+    @Autowired
+    public ApiController(CarService service, TypeCarService typesCarService, SpeedService speedService) {
+        this.service = service;
+        this.typesCarService = typesCarService;
+        this.speedService = speedService;
+    }
+
     @GetMapping("/cars")
-    public ResponseEntity<List<? extends CarInfo>> getCars(CarRequestParameters requestParameters) {
-        return ResponseEntity.ok(emptyList());
+    public ResponseEntity<List<? extends CarInfo>> getCars(@Valid CarRequestParameters requestParameters) {
+        List<? extends CarInfo> result = service.getCars(requestParameters);
+        return result.isEmpty()? ResponseEntity.notFound().build() : ResponseEntity.ok(service.getCars(requestParameters));
     }
 
     @GetMapping("/fuel-types")
     public ResponseEntity<List<String>> getFuelTypes() {
-        return ResponseEntity.ok(emptyList());
+        return ResponseEntity.ok(typesCarService.getFuelTypes());
     }
 
     @GetMapping("/body-styles")
     public ResponseEntity<List<String>> getBodyStyles() {
-        return ResponseEntity.ok(emptyList());
+        return ResponseEntity.ok(typesCarService.getBodyStyles());
     }
 
     @GetMapping("/engine-types")
     public ResponseEntity<List<String>> getEngineTypes() {
-        return ResponseEntity.ok(emptyList());
+        return ResponseEntity.ok(typesCarService.getEngineTypes());
     }
 
     @GetMapping("/wheel-drives")
     public ResponseEntity<List<String>> getWheelDrives() {
-        return ResponseEntity.ok(emptyList());
+        return ResponseEntity.ok(typesCarService.getWheelDrives());
     }
 
     @GetMapping("/gearboxes")
     public ResponseEntity<List<String>> getGearboxTypes() {
-        return ResponseEntity.ok(emptyList());
+        return ResponseEntity.ok(typesCarService.getGearboxTypes());
     }
 
     @GetMapping("/max-speed")
     public ResponseEntity<Double> getMaxSpeed(MaxSpeedRequestParameters requestParameters) {
-        return ResponseEntity.ok(0.0);
+        if ((requestParameters.getBrand() == null && requestParameters.getModel() == null) ||
+                (requestParameters.getBrand() != null && requestParameters.getModel() != null)) {
+            return ResponseEntity.badRequest().build();
+        }
+        Double result = speedService.getAverageMaxSpeed(requestParameters);
+
+        return result.equals(-1.0)? ResponseEntity.notFound().build() : ResponseEntity.ok(speedService.getAverageMaxSpeed(requestParameters));
     }
 }
